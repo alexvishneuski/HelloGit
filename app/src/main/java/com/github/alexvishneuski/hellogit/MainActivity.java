884b0117262ccc9ab926443dd8599669fc95ae1f
alexvishneuski <alexvishneuski@gmail.com>
2017-10-08 02:10:47 +0300

    Commit #1 in an yet separate branch before rebase

--- app/src/main/java/com/github/alexvishneuski/hellogit/MainActivity.java
+++ app/src/main/java/com/github/alexvishneuski/hellogit/MainActivity.java
@@ -6,41 +6,10 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate is called");
-        //Some changes in branch develop
-        //First feature improvement
-        //Second feature improvement: сonflict before merge is simulated in this line
-        //Some changes in branch develop
-        //Added formal changes in branch release-1.0
-        //Added feature to branch feature_third_feature
-
-
-
-
-
-
-
-
-
-        //Added some changes to show "CheryPick!!!"
-        //Added feature to branch feature_third_feature
-
-
-
-
-
-
-
-
-
-        //Added some changes to show "CheryPick!!!"
-
-        //Hidden changes after stashing - unStashing
+        //Commit #1 in an yet separate branch before rebase
     }
 }
