aa447ed1973cf8a70c3bb8a5688a0c8b4971de4c
alexvishneuski <alexvishneuski@gmail.com>
2017-10-07 03:30:35 +0300

    Second feature improvement

--- app/src/main/java/com/github/alexvishneuski/hellogit/MainActivity.java
+++ app/src/main/java/com/github/alexvishneuski/hellogit/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Some changes in branch develop
-        //Added branch feature_second_feature
+        //Second feature improvement
     }
 }
