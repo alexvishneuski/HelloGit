413bca7d8423a1e3d1bd7eb57903d03bff29e8b1
alexvishneuski <alexvishneuski@gmail.com>
2017-10-07 06:11:37 +0300

    Hidden changes after stashing - unStashing

--- app/src/main/java/com/github/alexvishneuski/hellogit/MainActivity.java
+++ app/src/main/java/com/github/alexvishneuski/hellogit/MainActivity.java
@@ -22,6 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Added some changes to show "CheryPick!!!"
 
-        //Added some changes before "Stash"
+        //Hidden changes after stashing - unStashing
     }
 }
